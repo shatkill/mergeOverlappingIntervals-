3530bb2695a5be4d861dcf095b757dd44d833b54
Marvin Funke <marvin.funke@fdmgroup.com>
2021-04-14 23:49:09 +0200

    MainDriver: delete unnecessary comparison for full interval overlap in merge method, add comments

--- src/main/java/mergeOverlappingIntervals/MainDriver.java
+++ src/main/java/mergeOverlappingIntervals/MainDriver.java
@@ -11,17 +11,14 @@ public class MainDriver {
 	
 	public static void main(String[] args) {
 		
+		// example calculation
+		
 		// initialize input as list of intervals
 		List<Interval> input = new ArrayList<Interval>();
-//		input.add(new Interval(25,30));
-//		input.add(new Interval(2, 19));
-//		input.add(new Interval(14, 23));
-//		input.add(new Interval(4, 8));
-		
-		input.add(new Interval(1,4));
-		input.add(new Interval(7, 8));
-		input.add(new Interval(5, 6));
-		input.add(new Interval(3, 4));		
+		input.add(new Interval(25,30));
+		input.add(new Interval(2, 19));
+		input.add(new Interval(14, 23));
+		input.add(new Interval(4, 8));	
 		
 		// prints merged interval list to screen
 		System.out.println(merge(input));		
@@ -47,10 +44,8 @@ public class MainDriver {
 			/* check for overlap */
 			if(mergedIntervalStack.peek().getEnd() >= inputList.get(i).getBegin()) {
 				
-				/* check for total overlap */
-				if(mergedIntervalStack.peek().getEnd() >= inputList.get(i).getEnd()) {
-					continue;
-				} else {					
+				/* check if has to be merged */
+				if(mergedIntervalStack.peek().getEnd() < inputList.get(i).getEnd()) {			
 					Interval temp = new Interval(mergedIntervalStack.peek().getBegin(), inputList.get(i).getEnd());
 					mergedIntervalStack.pop();
 					mergedIntervalStack.push(temp);
@@ -59,10 +54,10 @@ public class MainDriver {
 				mergedIntervalStack.push(inputList.get(i));
 			}
 		}
-				
+						
+		/* optional - convert result stack back to list */
 		List<Interval> outputList = new ArrayList<Interval>();
 		
-		/* optional - convert result stack back to list */
 		for(Interval i : mergedIntervalStack) {
 			
 			outputList.add(i);
